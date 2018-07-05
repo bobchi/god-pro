25c2e1389c04039321cbb4f1ff90dffe3cd98100
bobchi <bobchi@163.com>
2018-07-04 17:13:32 -0700

    code optimisation

--- bao/orm.php
+++ bao/orm.php
@@ -4,31 +4,64 @@ class orm
 {
     public $sql = [
         'select' => 'select ',
-        'from' => ' from '
+        'from' => ['from ',[]]
     ];
+
     function select(){
         $fields = func_get_args();
         foreach ($fields as $field){
-            if(trim($this->sql[__FUNCTION__]) != __FUNCTION__)
-            $this->sql[__FUNCTION__] .= ',';
-            $this->sql[__FUNCTION__] .= $field;
+            $this->_add(__FUNCTION__,$field);
         }
         return $this;
     }
 
-    function from(){
+    function from()
+    {
         $fields = func_get_args();
         foreach ($fields as $field){
-            if(trim($this->sql[__FUNCTION__]) != __FUNCTION__)
-                $this->sql[__FUNCTION__] .= ',';
-            $this->sql[__FUNCTION__] .= $field;
+            $this->_add(__FUNCTION__,$field);
         }
 //        $this->sql[__FUNCTION__] .= $tableName;
         return $this;
     }
 
+    function _add($key, $field, $split=',')
+    {
+        if(!$this->sql) return;
+        if(is_array($this->sql[$key]))
+        {
+            if(!in_array($field, $this->sql[$key][1]))
+            {
+                $this->sql[$key][1][] = $field;
+            }
+        }
+        else
+        {
+            if(trim($this->sql[$key]) != $key)
+                $this->sql[$key] .= $split;
+            $this->sql[$key] .= $field;
+        }
+    }
+
     function __toString(){
-        return implode($this->sql);
+//        return implode($this->sql);
+        $map = function ($items)
+        {
+          if(!is_array($items)) {return $items;}
+          else
+          {
+              $res = '';
+              foreach ($items[1] as $item)
+              {
+                  if($res != '') $res .= ',';
+                  $res .= $item;
+              }
+              return $items[0] . $res;
+          }
+        };
+
+        $ret = array_map($map, array_values($this->sql));
+        return implode($ret,' ');
     }
 }
 
